11e70ba73e3fdfffa31e2d48d4f82e7d2c77b22f
Zin Andriy (Contractor) <mc91138@external.mcd.com>
2020-01-16 22:39:36 -0600

    added file

---
+++ vars/copy_bin_to_wksp.groovy
@@ -0,0 +1,22 @@
+def inputGetFile(String savedfile = null) {
+    def filedata = null
+    def filename = null
+    // Get file using input step, will put it in build directory
+    // the filename will not be included in the upload data, so optionally allow it to be specified
+
+    if (savedfile == null) {
+        def inputFile = input message: 'Upload file', parameters: [file(name: 'library_data_upload'), string(name: 'restaurantCSV', defaultValue: 'My.csv')]
+        filedata = inputFile['library_data_upload']
+        filename = inputFile['restaurantCSV']
+    } else {
+        def inputFile = input message: 'Upload file', parameters: [file(name: 'library_data_upload')]
+        filedata = inputFile
+        filename = savedfile
+    }
+
+    // Read contents and write to workspace
+    writeFile(file: filename, encoding: 'Base64', text: filedata.read().getBytes().encodeBase64().toString())
+    // Remove the file from the master to avoid stuff like secret leakage
+    filedata.delete()
+    return filename
+}
